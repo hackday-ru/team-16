496fe90cab315f275c644350c067460dbb1eee89
Artem Nurgaliev <artem_nurgaliev@epam.com>
2016-04-16 23:04:31 +0300

    LAN Game discovery implementation

---
+++ UnityProject/Assets/Scripts/LANDiscoverController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class LANDiscoverController : NetworkDiscovery {
+	public override void OnReceivedBroadcast (string fromAddress, string data)
+	{
+		//StopBroadcast ();
+
+		Debug.Log ("Connecting: " + fromAddress);
+		NetworkManager.singleton.StopHost();
+		NetworkManager.singleton.networkAddress = fromAddress;
+
+		Destroy (this.gameObject);
+	}
+
+	void OnDestroy()
+	{
+		NetworkManager.singleton.StartClient();
+	}
+}
---
+++ UnityProject/Assets/Scripts/ServerSide.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class ServerSide : NetworkBehaviour, iNetBehaviour {
+
+	public NetworkDiscovery networkDiscovery;
+	//private NetworkManager netManager;
+
+	// Use this for initialization
+	public void Initialize ()
+	{
+		networkDiscovery = GetComponentInParent<NetworkDiscovery> ();
+		networkDiscovery.Initialize ();
+		networkDiscovery.StartAsServer ();
+		Debug.Log ("**** Server started ****");
+
+		//netManager = NetworkManager.singleton;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	[Command]
+	void CmdClientReady()
+	{
+
+	}
+
+//	void OnGUI() {
+//		GUI.Label(new Rect(100, 100, 100, 100), "Server started");
+//	}
+}
