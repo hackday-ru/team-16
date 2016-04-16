9c90c1e581b47cb8aa5e5d538cedc6357c4fb83a
pavel_lysenko <pavel_lysenko@epam.com>
2016-04-16 21:50:55 +0300

    game manager added

--- UnityProject/Assets/Scripts/VehicleController.cs
+++ UnityProject/Assets/Scripts/VehicleController.cs
@@ -16,22 +16,32 @@ public class VehicleController : MonoBehaviour
     public float maxSteering;
     public Axle[] axles = {};
 
+	private bool powered = false;
+
+	public void SelectVechicle(){
+		gameObject.GetComponent<Rigidbody> ().useGravity = true;
+	}
+
+	public void StartEngine(){
+		powered = true;
+	}
+
     public void FixedUpdate()
     {
-        float torque = maxTorque * Input.GetAxis("Vertical");
-        float steering = maxSteering * Input.GetAxis("Horizontal");
+		if (powered) {
+			float torque = maxTorque * Input.GetAxis ("Vertical");
+			float steering = maxSteering * Input.GetAxis ("Horizontal");
 
-        foreach (var axle in axles)
-        {
-            axle.left.steerAngle = steering * axle.steeringFactor;
-            axle.right.steerAngle = steering * axle.steeringFactor;
+			foreach (var axle in axles) {
+				axle.left.steerAngle = steering * axle.steeringFactor;
+				axle.right.steerAngle = steering * axle.steeringFactor;
 
-            if (axle.applyTorque)
-            {
-                axle.left.motorTorque = torque;
-                axle.right.motorTorque = torque;
-            }
-        }
+				if (axle.applyTorque) {
+					axle.left.motorTorque = torque;
+					axle.right.motorTorque = torque;
+				}
+			}
+		}
     }
 
 
