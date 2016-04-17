ba0ab16b530009f6050c0753b082462c896ada56
Evangelion001 <mishaworkgroup@gmail.com>
2016-04-17 12:21:52 +0300

    Added big hud

---
+++ UnityProject/Assets/Scripts/Hud.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Hud : MonoBehaviour {
+
+    public InputField inputField;
+    public Button startClient;
+
+    public void Start () {
+        startClient.onClick.AddListener( () => {
+            NetworkManager.singleton.networkAddress = inputField.text;
+            NetworkManager.singleton.StartClient();
+            Destroy( startClient.gameObject );
+            Destroy( inputField.gameObject );
+        } );
+    }
+
+
+
+}
